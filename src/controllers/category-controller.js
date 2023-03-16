1fc5d81d8c5ba347b3240c9dceb5cee2a766d7e7
Piotr Bielski <piotrbielski3@gmail.com>
2023-03-16 20:03:51 +0000

    Delete Category and POI feature

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -25,4 +25,12 @@ export const categoryController = {
       return h.redirect(`/category/${category._id}`);
     },
   },
+
+  deletePoi: {
+    handler: async function(request, h) {
+      const category = await db.categoryStore.getCategoryById(request.params.id);
+      await db.poiStore.deletePoi(request.params.poiId);
+      return h.redirect(`/category/${category._id}`);
+    },
+  },
 };
