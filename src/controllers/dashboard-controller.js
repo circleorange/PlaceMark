40377949b0f812ce1403135afce09a1431cc890d
Piotr Bielski <piotrbielski3@gmail.com>
2023-03-16 18:36:50 +0000

    Accounts Authentication

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -3,9 +3,11 @@ import { db } from "../models/db.js";
 export const dashboardController = {
   index: {
     handler: async function (request, h) {
-      const categories = await db.categoryStore.getAllCategories();
+      const loggedInUser = request.auth.credentials;
+      const categories = await db.categoryStore.getUserCategories(loggedInUser._id);
       const viewData = {
         title: "PlaceMark Dashboard",
+        user: loggedInUser,
         categories: categories,
       };
       return h.view("dashboard-view", viewData);
@@ -14,8 +16,10 @@ export const dashboardController = {
 
   addCategory: {
     handler: async function (request, h) {
+      const loggedInUser = request.auth.credentials;
       const newCategory = {
-        category: request.payload.category,
+        userid: loggedInUser._id,
+        type: request.payload.type,
       };
       await db.categoryStore.addCategory(newCategory);
       return h.redirect("/dashboard");
