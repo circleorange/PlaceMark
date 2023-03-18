f8267f16b758e299c78337c2ce52055d0ab4f628
Piotr Bielski <piotrbielski3@gmail.com>
2023-03-18 14:59:49 +0000

    User, Category, POI tests
    Includes fixes to JSON models

--- src/models/json/poi-json-store.js
+++ src/models/json/poi-json-store.js
@@ -4,13 +4,11 @@ import { JSONFile } from "lowdb/node";
 
 const db = new Low(new JSONFile("./src/models/json/pois.json"));
 db.data = { pois: [] };
-
 export const poiJsonStore = {
   async getAllPois() {
     await db.read();
     return db.data.pois;
   },
-
   async addPoi(categoryId, poi) {
     await db.read();
     poi._id = v4();
@@ -19,29 +17,26 @@ export const poiJsonStore = {
     await db.write();
     return poi;
   },
-
   async getPoisByCategoryId(id) {
     await db.read();
     return db.data.pois.filter((poi) => poi.categoryid === id);
   },
-
   async getPoiById(id) {
     await db.read();
-    return db.data.pois.find((poi) => poi._id === id);
+    const poi = db.data.pois.find((poi) => poi._id === id);
+    if (poi) { return poi }
+    return null
   },
-
   async deletePoi(id) {
     await db.read();
     const index = db.data.pois.findIndex((poi) => poi._id === id);
-    db.data.pois.splice(index, 1);
+    if (index !== -1) db.data.pois.splice(index, 1);
     await db.write();
   },
-
   async deleteAllPois() {
     db.data.pois = [];
     await db.write();
   },
-
   async updatePois(poi, updatedPoi) {
     poi.name = updatedPoi.name;
     poi.description = updatedPoi.description;
